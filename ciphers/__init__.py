0ef90140cf763b046a05a266a0d397c0c18feba8
Loulou <104641024+sioulladiv@users.noreply.github.com>
2024-12-11 00:08:37 +0000

    Update __init__.py

--- ciphers/__init__.py
+++ ciphers/__init__.py
@@ -2,5 +2,6 @@ from .vigenere import VigenereCracker
 from .substitution import SubstitutionCracker
 from .shuffle import ShuffleCracker
 from .polybius import PolybiusCracker
+from .playfair import PlayfairCracker
 
-__all__ = ['VigenereCracker', 'SubstitutionCracker', 'ShuffleCracker', 'PolybiusCracker']
+__all__ = ['VigenereCracker', 'SubstitutionCracker', 'ShuffleCracker', 'PolybiusCracker','PlayfairCracker']
